615cab41c2972708899fc70d3e52f9beec7dbbb1
Likith-dr <rajumpel@gmail.com>
2025-04-12 09:21:29 +0000

    done

--- basics.js
+++ basics.js
@@ -1,1 +1,39 @@
 console.log("Welcome");
+/*
+"use strict "; it treats all js code as new version
+we can not use alert here because we are not in browser
+tc39 and mbm is important documents for the js
+
+DATA-TYPES:
+primitive types:
+>> number = 2 to power 53
+>> bigint = for big numbers
+>> string = ""
+>> boolean = true/false
+>> null => standalone value
+>> undefined =
+> symbol = used for uniqueness
+
+non-primitive type
+>>object 
+
+
+typeOf is used to know the type of data 
+but null is a type of object
+and undefined is type of undefined
+it is important 
+
+
+CONVERSION AND OPERATIONS:
+
+*/
+console.log(3 + 3)
+console.log("bye")
+
+let score = "33abs"
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber) // NaN == not a number 
